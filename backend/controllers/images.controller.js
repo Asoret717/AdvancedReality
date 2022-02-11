c65d7baa63817caec1ec0ed9d424fdf6fde889d1
Asoret717 <asoretoverpower@gmail.com>
2022-02-11 19:18:41 +0000

    Full javascript Crud

--- backend/controllers/images.controller.js
+++ backend/controllers/images.controller.js
@@ -7,7 +7,7 @@ exports.create = (req, res) => {
   // Validate request
   if (!req.body.name || !req.body.fileName || !req.body.place) {
     res.status(400).send({
-      message: "Content can not be empty!"
+      message: "Content of image is empty!"
     });
     return;
   }
